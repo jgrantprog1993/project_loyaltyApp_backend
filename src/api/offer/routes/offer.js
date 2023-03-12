13dad2a8be977e7b123c269cf508fcbc469c1c59
jgrantprog1993 <jason.grant@umail.ucc.ie>
2023-03-12 22:59:46 +0000

    heroku update

--- src/api/offer/routes/offer.js
+++ src/api/offer/routes/offer.js
@@ -4,6 +4,20 @@
  * offer router
  */
 
+module.exports = {
+    routes: [
+      {
+       method: 'POST',
+       path: '/offer',
+       handler: 'offer.create',
+       config: {
+         policies: [],
+         middlewares: [],
+       },
+      },
+    ],
+  };
+
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
 module.exports = createCoreRouter('api::offer.offer');
--- src/api/offer/services/offer.js
+++ src/api/offer/services/offer.js
@@ -5,5 +5,22 @@
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::offer.offer');
+const toEmail = process.env.TOEMAIL;
+const welcome = process.env.WELCOME;
+//module.exports = createCoreService('api::offer.offer');
+module.exports = {
+    async create(data) {
+      const response = await strapi.entityService.create('api::offer.offer', {
+        data,
+      });
+      strapi
+        .service("api::comment.sendmail")
+        .send(
+          welcome,
+          toEmail,
+          "Welcome",
+          `A Offer has been created ${entity.name}`
+        );
+      return response;
+    },
+  };

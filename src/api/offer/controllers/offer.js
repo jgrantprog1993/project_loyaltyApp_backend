d23f5c0ef16a8abc335879c8e27560183581760c
jgrantprog1993 <jason.grant@umail.ucc.ie>
2023-03-13 01:24:17 +0000

    updated offers.js

--- src/api/offer/controllers/offer.js
+++ src/api/offer/controllers/offer.js
@@ -10,10 +10,5 @@ module.exports = createCoreController('api::offer.offer', {
     count(ctx) {
        var { query } = ctx.request
        return strapi.query('api::offer.offer').count({ where: query });
-   },
-   async create(ctx) {
-    return await strapi
-      .service("api::offer.offer")
-      .create(ctx.request.body);
-  },
+   }
  });
--- src/api/offer/services/offer.js
+++ src/api/offer/services/offer.js
@@ -5,22 +5,5 @@
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
-const toEmail = process.env.TOEMAIL;
-const welcome = process.env.WELCOME;
-//module.exports = createCoreService('api::offer.offer');
-module.exports = {
-    async create(data) {
-      const response = await strapi.entityService.create('api::offer.offer', {
-        data,
-      });
-      strapi
-        .service("api::comment.sendmail")
-        .send(
-          welcome,
-          toEmail,
-          "Welcome",
-          `A Offer has been created ${entity.name}`
-        );
-      return response;
-    },
-  };
+
+module.exports = createCoreService('api::offer.offer');
